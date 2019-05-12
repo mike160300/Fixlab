88668c28e07eaaf196446f9df78729cc5d939ab6
mariocata <44073884+mariocata@users.noreply.github.com>
2019-05-12 11:40:05 -0400

    Add files via upload

---
+++ server/build/keys.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'bwbf4vkssvgg0zyrnqpp-mysql.services.clever-cloud.com',
+        user: 'ujnri668cw9fgrsm',
+        password: 'v6mMskdlrNUWOshFcQ9A',
+        database: 'bwbf4vkssvgg0zyrnqpp'
+    }
+};
