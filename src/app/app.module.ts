e1a9d643f92b280015f76e3b9ba9412c9463527e
Cesar Salazar <cesarsalazarlopez25@gmail.com>
2019-05-16 15:17:55 -0400

    Componentes de inicio de sesión y dashboard creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,27 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+//Módulos:
+import { AuthModule } from './auth/auth.module';
+
+//Servicios:
+
+//Componentes de Ngx-Bootstrap:
+
+
+//Componentes:
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    AuthModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
