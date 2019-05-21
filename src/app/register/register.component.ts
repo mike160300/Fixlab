a56e63a09143b227c559ec047033ef1ae98cbe58
Cesar Salazar <cesarsalazarlopez25@gmail.com>
2019-05-21 17:55:14 -0400

    Cambios en html y css del login y registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -5,7 +5,7 @@ import { Router } from "@angular/router";
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['../login/login.component.css']
 })
 export class RegisterComponent {
   credentials: TokenPayload = {
