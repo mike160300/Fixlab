3ab16239d47429c0467dabf2e6ee98c4aa784943
mariocata <44073884+mariocata@users.noreply.github.com>
2019-05-24 07:41:42 -0400

    Add files via upload

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent {
   register(form: NgForm) {
     this.auth.register(this.credentials).subscribe(
       () => {
-        this.router.navigateByUrl("/profile");
+        this.router.navigateByUrl("/home");
       },
       err => {
         console.error(err);
