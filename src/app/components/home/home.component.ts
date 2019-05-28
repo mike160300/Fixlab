15667a374638dc7888f9234f367739d9930fbdaf
Cesar Salazar <cesarsalazarlopez25@gmail.com>
2019-05-28 00:46:55 -0400

    Cambios en navbar + arreglos varios

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import {AuthenticationService} from '../../auth/authentication.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  modalRef1: BsModalRef;
+  modalRef2: BsModalRef;
+  modalRef3: BsModalRef;
+  constructor(private modalService: BsModalService, private auth: AuthenticationService) {}
+ 
+  create(template: TemplateRef<any>) 
+  {
+    this.modalRef1 = this.modalService.show(template);
+    this.modalRef1.hide();
+  }
+
+  modify(template: TemplateRef<any>) 
+  {
+    this.modalRef2 = this.modalService.show(template);
+    this.modalRef2.hide();
+  }
+
+  delet(template: TemplateRef<any>) 
+  {
+    this.modalRef3 = this.modalService.show(template);
+    this.modalRef3.hide();
+  }
 
   ngOnInit() {
   }
