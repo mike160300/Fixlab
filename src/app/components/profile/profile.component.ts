286571046479459a4cb1c422b5c2ea882a9db106
mariocata <44073884+mariocata@users.noreply.github.com>
2019-06-04 15:41:37 -0400

    Add files via upload

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from '../../auth/authentication.service';
+import { Router } from "@angular/router";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -8,11 +10,23 @@ import {AuthenticationService} from '../../auth/authentication.service';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private auth: AuthenticationService) 
-  {
-   
-  }
+  credentials: UserProfile = {
+    description: ''
+  };
 
+  constructor(private auth: AuthenticationService,private router: Router) { }
+
+
+  editprofile(form: NgForm) {
+    this.auth.editprofile(this.credentials,this.auth.getUserDetails().id_user).subscribe(
+      () => {
+        this.router.navigateByUrl("/dashboard/home");
+      },
+      err => {
+        console.error(err);
+      }
+    ); ;
+  }
 
   ngOnInit() {
   }
